e6455eb82485bff20672728654fd70dafa4d31a1
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-19 15:34:53 -0300

    ligando para os candidatos

--- validacao-processo-seletivo/src/ProcessoSeletivo.java
+++ validacao-processo-seletivo/src/ProcessoSeletivo.java
@@ -1,8 +1,36 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        imprimirSelecionados();
+        String candidatos[] = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+        for(String candidato : candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");    
+
+        }while(continuarTentando && tentativasRealizadas< 3);
+        
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM "+ candidato + " NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas);    
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados (){
