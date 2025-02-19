1c42afc3d63307c4c803666eb671ab9f0e7550e8
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-19 15:17:50 -0300

    imprimindo os candidatos

--- validacao-processo-seletivo/src/ProcessoSeletivo.java
+++ validacao-processo-seletivo/src/ProcessoSeletivo.java
@@ -2,7 +2,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        selecaoCandidatos(); 
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados (){
+        String candidatos[] = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for(int indice = 0;indice < candidatos.length; indice++){
+            System.out.println("O candidato de numero " + (indice+1) + " é o " + candidatos[indice]);
+        }
+
     }
     
     static void selecaoCandidatos(){        
