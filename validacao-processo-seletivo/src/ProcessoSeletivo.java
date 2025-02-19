aaf2639373b51b00b5aed042f5ad99438a7a3724
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-19 15:10:04 -0300

    seleção de candidatos

--- validacao-processo-seletivo/src/ProcessoSeletivo.java
+++ validacao-processo-seletivo/src/ProcessoSeletivo.java
@@ -1,8 +1,31 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCandidatos(1900.0);
-        analisarCandidatos(2200.0);
-        analisarCandidatos(2000.0);       
+        selecaoCandidatos(); 
+    }
+    
+    static void selecaoCandidatos(){        
+        String candidatos[] = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String canditado = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + canditado + " Solicitou este valor de salario " + salarioPretendido);
+            if(salarioBase >=  salarioPretendido){
+                System.out.println("O candidato " + canditado + " Foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2000);
     }
 
     static void analisarCandidatos(double salarioPretendido){
