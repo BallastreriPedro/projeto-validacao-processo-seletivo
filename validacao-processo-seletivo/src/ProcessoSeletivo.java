6a6e85129d0c81fbb14092962b437ee885e6603c
Pedro Ballastreri <gballastreri@gmail.com>
2025-02-19 14:49:16 -0300

    analisando oss candidatos

--- validacao-processo-seletivo/src/ProcessoSeletivo.java
+++ validacao-processo-seletivo/src/ProcessoSeletivo.java
@@ -1,5 +1,19 @@
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        
+        analisarCandidatos(1900.0);
+        analisarCandidatos(2200.0);
+        analisarCandidatos(2000.0);       
+    }
+
+    static void analisarCandidatos(double salarioPretendido){
+        double salarioBase = 2000.0;
+
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDANDO DEMAIS CANDITADOS");
+        }
     }
 }
